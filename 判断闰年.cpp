904e0ccb5b497158f6e6699682cd56b26e6e5b73
Waylon06 <76614752+Waylon06@users.noreply.github.com>
2021-03-06 22:31:56 +0800

    Add files via upload

---
+++ 判断闰年.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	char year,month,day,sum;
+	printf("请输入year的值：\n");
+	scanf("%d",&year);
+	if(year%4==0)
+	{
+	    if(year%100==0){
+		    printf("该年为非闰年\n"); 
+	        if(year%400==0)
+	            printf("该年为闰年\n");
+		    else
+		        printf("该年为非闰年\n");
+		} 
+	    else
+	        printf("该年为闰年\n");}
+	else
+	    printf("该年为非闰年\n");
+	printf("请输入month与day的值：\n");
+	scanf("%d %d",&month,&day);
+	sum=30*month+day;
+	printf("这是该年的第%d天",sum); 
+	return 0; 
+}
