485433972e667efb0e32bd7c87a659c41ef424b9
Waylon06 <76614752+Waylon06@users.noreply.github.com>
2021-03-09 21:50:06 +0800

    Add files via upload

---
+++ 存取款.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<windows.h>
+void save();							/*存钱函数*/
+void draw();					/*取钱函数*/
+double count,balance=0;							/*全局变量，表示账户余额*/
+/******************************主函数*****************************/
+int main()
+{
+	int a;
+	while(1)							/*使用循环结构完成菜单循环*/
+	{							/*菜单不显示隐藏功能*/ 
+		printf("\t\t\t\t\t|--------存钱取钱程序菜单-------|\n");
+		printf("\t\t\t\t\t|\t1:存钱\t\t\t|\n");
+		printf("\t\t\t\t\t|\t2:取钱\t\t\t|\n");
+		printf("\t\t\t\t\t|\t0:退出\t\t\t|\n");
+		printf("\t\t\t\t\t|-------------------------------|\n");
+		scanf("%d",&a);
+		switch(a)
+		{
+			case 1:save();break;
+			case 2:draw();break;
+			case 0:printf("服务关闭");
+			    return 0;
+			    break; 
+			default:
+				printf("抱歉您的输入有误，请重新输入");
+				break; 
+		}
+		//补充程序
+
+
+	}
+	return 0;
+}
+/**************************存钱处理函数************************/
+void save()
+{
+	printf("**************************存款程序**************************\n");
+	printf("请输入存款金额：");
+	scanf("%lf",&count);  
+	balance+=count;
+	printf("您的余额为:%.2lf\n",balance);
+	system("pause");
+	system("cls");
+}
+/******************************取钱处理函数*******************/
+void draw()
+{
+	printf("**************************取款程序**************************\n"); 
+	printf("请输入取款金额：");
+	scanf("%lf",&count);
+	balance-=count;
+	printf("您的余额为：%.2lf\n",balance);
+	system("pause");
+	system("cls");
+}
+
