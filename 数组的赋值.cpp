80e7bb74fffcb7af924b61166474e55d75959c63
Waylon <205218290@qq.com>
2021-03-19 23:00:03 +0800

    .\数组的赋值.cpp

---
+++ 数组的赋值.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int a[10]={0};
+	int i;
+	
+	for(i=0;i<10;i++)
+	{
+		printf("%d\n",a[i]);
+	} 
+	
+	int b[10]={1,2,3,4,5,6}; 
+	int l;
+	
+	for(l=0;l<10;l++)
+	{
+		printf("%d\n",b[l]);
+	}
+	
+	return 0;
+}
